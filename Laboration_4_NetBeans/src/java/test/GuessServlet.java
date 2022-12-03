7c2eded4dbbfdb66456a9e8a0eecbcfbc3f054dc
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2022-12-03 12:08:56 +0100

    Now it works :-D

---
+++ Laboration_4_NetBeans/src/java/test/GuessServlet.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Gustav Normelli
+ */
+@WebServlet(name = "GuessServlet", urlPatterns = {"/GuessServlet"})
+public class GuessServlet extends HttpServlet {
+
+
+//    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//        HttpSession session = req.getSession(true);
+//        Model model;
+//        int guessInt = Integer.parseInt(req.getParameter("guess"));
+//        if(session.isNew()) {
+//                System.out.print("I am a new session");
+//                model = new Model();
+//                session.setAttribute("model", model);
+//
+//        }
+//    }
+
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        HttpSession session = req.getSession(true);
+        PrintWriter out = res.getWriter();
+        Model model;
+        
+        if(session.isNew()) {
+            model = new Model();
+            model.setGuess(-1);  
+            session.setAttribute("model", model);            
+        }else {    
+            model = (Model) session.getAttribute("model");
+            int guessInt = Integer.parseInt(req.getParameter("guess"));
+            model.setGuess(guessInt);
+            model.incrementNumberOfGuesses();
+        }
+        out.print(model.createTheMessage());
+        //Set the content type 
+        res.setContentType("text/html");
+        RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
+        rd.include(req, res);
+    }
+}
---
+++ Laboration_4_NetBeans/web/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Guess a number</title>
+    </head>
+<body>
+    <form action="guess" method="get">
+        <input type="text" name="guess"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
