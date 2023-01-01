7f18b9f2ffed7544db2b18af3081aef6c1c38d4d
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2023-01-01 14:40:14 +0100

    Refactoring

--- Project/ID1212_Project_Intelij/src/main/java/com/ID1212/ID1212_Project_Intelij/Controller/MessageController.java
+++ Project/ID1212_Project_Intelij/src/main/java/com/ID1212/ID1212_Project_Intelij/Controller/MessageController.java
@@ -19,20 +19,6 @@ public class MessageController {
     @Autowired
     MessageService messageService;
 
-//    @PostMapping(name = "/messages_from_conv")
-//    public Object getMessagesFromConversationID(@RequestBody Conversation conversation){
-//        Long conID = conversation.getId();
-//        Collection<Message> messages = messageService.getAllMessagesForConversationID(conID);
-//        if(messages.isEmpty()){
-//            HashMap<String, String> map = new HashMap<>();
-//            map.put("id", "Conversation does not exist");
-//            return map;
-//        }else{
-//            return messages;
-//        }
-//
-//    }
-
     @PostMapping("/send_message")
     public Object takeNewMessage(@RequestBody Message message){
         messageService.createNewMessage(message);
@@ -48,19 +34,4 @@ public class MessageController {
         return conversationMessages;
     }
 
-
-    @PostMapping("/new_message")
-    public Object createNewMessage(@RequestBody Message message){
-//        if(message.getMessageText() == null){
-//            HashMap<String, String> map = new HashMap<>();
-//            map.put("id", "message was not posted.");
-//            return map;
-//        }else{
-//            LocalDateTime dateTime = LocalDateTime.now();
-//            message.setSentDateTime(dateTime);
-//            messageService.createNewMessage(message);
-//            return "message was added";
-//        }
-        return null;
-    }
 }
--- Project/ID1212_Project_Intelij/src/main/java/com/ID1212/ID1212_Project_Intelij/Service/MessageService.java
+++ Project/ID1212_Project_Intelij/src/main/java/com/ID1212/ID1212_Project_Intelij/Service/MessageService.java
@@ -6,36 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class MessageService {
 private final MessageRepository messageRepository;
-
-
     @Autowired
     public MessageService(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
     }
 
-//    public Collection<Message> getAllMessagesForConversationID(Long convId){
-//        return messageRepository.findMessagesByFk_conversationIs(convId);
-//    }
-
     public void createNewMessage(Message message) {
         message.setSentDateTime(LocalDateTime.now());
         messageRepository.save(message);
     }
-
     public List <Message> getMessagesByConversationId(Long id) {
-
-        List<Message> h = messageRepository.findAll();
-
-        List<Message> result = h.stream().filter(msg -> msg.getFk_conversation().getId().equals(id)).collect(Collectors.toList());  ;
-
+        List<Message> allMessagesFromRepository = messageRepository.findAll();
+        List<Message> result = allMessagesFromRepository.stream().filter(msg ->
+                msg.getFk_conversation().getId().equals(id)).collect(Collectors.toList());
         return result;
-
     }
 }
